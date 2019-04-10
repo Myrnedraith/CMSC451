a1e51e3a597a0ecc0fa390cfb6092a4b303be9b1
Marazair <nick1022m@gmail.com>
2019-04-10 13:02:22 -0400

    Benchmark class and BenchmarkSorts constructor
    Created a Benchmark class for storing and manipulating
    information pertaining to each benchmark.
    
    Created constants to contain the different lengths and
    the number of tests for each length.
    
    Added an ArrayList for storing the different benchmarks.

--- Project1/src/main/BenchmarkSorts.java
+++ Project1/src/main/BenchmarkSorts.java
@@ -1,8 +1,38 @@
 package main;
 
+import java.util.*;
+
 public class BenchmarkSorts {
-	public BenchmarkSorts(int[] sizes) {
+	private static final int SETS_PER_SIZE = 50;
+	
+	private ArrayList<Benchmark> benchmarks = new ArrayList<Benchmark>();
+	
+	class Benchmark {
+		int size;
+		String type;
 		
+		int opCount;
+		int countVar;
+		int time;
+		int timeVar;
+		
+		Benchmark(int size, String type) {
+			this.size = size;
+			this.type = type;
+			
+			opCount = 0;
+			countVar = 0;
+			time = 0;
+			timeVar = 0;
+		}
+	}
+	
+	
+	public BenchmarkSorts(int[] sizes) {
+		for(int i = 0; i < sizes.length; i++) {
+			benchmarks.add(new Benchmark(sizes[i], "Recursive"));
+			benchmarks.add(new Benchmark(sizes[i], "Iterative"));
+		}
 	}
 	
 	public void runSorts() {
--- Project1/src/main/SortMain.java
+++ Project1/src/main/SortMain.java
@@ -1,7 +1,9 @@
 package main;
 
 public class SortMain {
-
+	private static final int[] LENGTHS = {10, 20, 50, 100, 200, 500, 
+			1000, 5000, 10000, 50000};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
